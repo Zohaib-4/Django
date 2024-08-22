cd129427067838657b321c88f01c5d52b4dcfdba
Zohaib <sameer.zohaib363@gmail.com>
2024-08-22 20:34:08 +0500

    Practice and Practice

---
+++ Templates_Inheritence/dbPractice/dbprac/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class dbase(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    age = models.IntegerField()
