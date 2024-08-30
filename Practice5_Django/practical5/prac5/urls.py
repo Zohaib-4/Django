375e0742449c0cebb748e811664b5a64bf65cc94
Zohaib <sameer.zohaib363@gmail.com>
2024-08-30 20:45:20 +0500

    added template

--- Practice5_Django/practical5/prac5/urls.py
+++ Practice5_Django/practical5/prac5/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('prac5/', views.prac5, name='prac5'),
+    path('login/', views.login, name='login')
 
 ]
--- Practice5_Django/practical5/prac5/views.py
+++ Practice5_Django/practical5/prac5/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import checkdb
 
 def prac5(request):
     return HttpResponse("This is Practical#5 of Django")
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        print(email)
+        
+        checkdb.objects.create(
+            email = email,
+            password = password
+        )
+
+    return render(request, 'login.html')
+
+def success(request):
+    return HttpResponse("All set good to go")
