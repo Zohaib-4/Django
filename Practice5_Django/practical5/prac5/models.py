5d93acef7816657dbd1eac21386ceb8d6331a5e8
Zohaib <sameer.zohaib363@gmail.com>
2024-08-30 20:14:20 +0500

    .

--- Practice5_Django/practical5/prac5/models.py
+++ Practice5_Django/practical5/prac5/models.py
@@ -5,4 +5,5 @@ class checkdb(models.Model):
     email = models.EmailField(max_length=255, null=False)
     password = models.CharField(max_length=255, null=False)
 
-    
+    def __str__(self):
+        return f"{self.email}"
