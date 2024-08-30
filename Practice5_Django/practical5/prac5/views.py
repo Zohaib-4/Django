06b1c13af4ce985f32613647a262d87869c9ee1a
Zohaib <sameer.zohaib363@gmail.com>
2024-08-30 21:34:41 +0500

    .

--- Practice5_Django/practical5/prac5/views.py
+++ Practice5_Django/practical5/prac5/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import checkdb
+import re
 
 def prac5(request):
     return HttpResponse("This is Practical#5 of Django")
 
 def login(request):
+    error_message = ""
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
 
         print(email)
-        
-        checkdb.objects.create(
-            email = email,
-            password = password
-        )
+
+        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        pass_pattern = r'^(?=.*[!@#$%^&*()_\-+={}[\]|\\:;"\'<>,.?/~`])(?=.*[A-Za-z])(?=.*\d).{8,}$'
+
+        if not re.match(email_pattern, email) or not re.match(pass_pattern, password) :
+            error_message = "Invalid email format or password format"
+        else:
+
+            checkdb.objects.create(
+                email = email,
+                password = password
+            )
         return redirect('success')
 
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'error_message': error_message})
 
 def success(request):
     return HttpResponse("All set good to go")
