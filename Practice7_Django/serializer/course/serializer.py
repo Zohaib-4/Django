7be840702f427594b9aa0c9c5326fff1b614282f
Zohaib <sameer.zohaib363@gmail.com>
2024-09-06 21:29:58 +0500

    /

--- Practice7_Django/serializer/course/serializer.py
+++ Practice7_Django/serializer/course/serializer.py
@@ -1,8 +1,19 @@
 
 from rest_framework import serializers
+from models import course_details, student_details
 
 class CourseSerializer(serializers.Serializer):
   title = serializers.CharField(max_length=255)
   description = serializers.CharField(max_length=255)
   instructor = serializers.CharField(max_length=255)
   duration = serializers.IntegerField()
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    roll = serializers.IntegerField()
+    phone = serializers.IntegerField()
+    email = serializers.EmailField()
+  
+class EnrollmentSerializer(serializers.Serializer):
+    course = serializers.PrimaryKeyRelatedField(course_details.objects.all())
+    student = serializers.PrimaryKeyRelatedField(student_details.objects.all())
