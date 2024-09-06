67e9d4d2c7426ff594cc24c5124eb232b9583673
Zohaib <sameer.zohaib363@gmail.com>
2024-09-06 20:11:34 +0500

    model

---
+++ Practice7_Django/serializer/course/models.py
@@ -0,0 +1,8 @@
+
+from django.db import models
+
+class course_details(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.CharField(max_length=255)
+  instructor = models.CharField(max_length=255)
+  duration = models.IntegerField()
