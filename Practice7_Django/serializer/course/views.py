1e58953483d833c006d95b608f18dfeb17a09f92
Zohaib <sameer.zohaib363@gmail.com>
2024-09-06 20:52:02 +0500

    .

--- Practice7_Django/serializer/course/views.py
+++ Practice7_Django/serializer/course/views.py
@@ -4,5 +4,15 @@ from .serializer import CourseSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
 
-def members(request):
-    return HttpResponse("Hello world!")
+
+def course_detail(request, pk):
+    course = course_details.objects.get(id=pk)
+    serializer = CourseSerializer(course)
+    jason_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(jason_data, content_type="application/json")
+
+def course_list(reqeust):
+    course = course_details.objects.all()
+    serializer = CourseSerializer(course, many=True)
+    jason_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(jason_data, content_type="application/json")
--- Practice7_Django/serializer/serializer/urls.py
+++ Practice7_Django/serializer/serializer/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('course.urls')),
     path('admin/', admin.site.urls),
+    path('course-info/<int:id>',views.course_details),
+    path('course-info/',views.course_list)
 ]
