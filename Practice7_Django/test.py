e16194be02f4f8147a46bfc28ae57dc1c52e56e5
Zohaib <sameer.zohaib363@gmail.com>
2024-09-13 19:41:27 +0500

    /

--- Practice7_Django/test.py
+++ Practice7_Django/test.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import requests
+import json
+
+URL = ""
+
+data = {
+    'roll' : 9090,
+    'name' : 'Rohan',
+    'city' : 'Sukhar'  
+}
+
+json_data = json.dump(data)
+
+r = requests.post(url = URL, data = json_data)
+data = r.json()
+print(data)
