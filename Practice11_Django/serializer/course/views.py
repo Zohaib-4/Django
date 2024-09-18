55dce740b6bb55747a38374a53c243c7a3848d4d
Zohaib <sameer.zohaib363@gmail.com>
2024-09-18 21:18:45 +0500

    /

--- Practice11_Django/serializer/course/views.py
+++ Practice11_Django/serializer/course/views.py
@@ -1,5 +1,6 @@
 import json
 from django.shortcuts import render
+from django.views import View
 from .models import course_details, student_details, enrollment
 from .serializer import CourseSerializer, StudentSerializer, EnrollmentSerializer
 from rest_framework.renderers import JSONRenderer
@@ -7,9 +8,109 @@ from django.http import HttpResponse, JsonResponse
 import io
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from django.utils.decorators import method_decorator
 
+@method_decorator(csrf_exempt, name='dispatch')
+class Course(View):
+    def get(self, request, pk):
+        course = course_details.objects.get(id=pk)
+        serializer = CourseSerializer(course)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type="application/json")
+    
+    def post(self, request):
+        data = request.body
+        stream = io.BytesIO(data)
+        pythonData = JSONParser().parse(stream)
+        serializer = CourseSerializer(data = pythonData)
 
-class Online
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Created'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        
+        json_data = JSONRenderer().render(serializer.error_messages)
+        return HttpResponse(json_data, content_type='application/json')
+
+    def put(self, request, course_id):
+        try:
+            course = course_details.objects.get(id=course_id)
+        except course_details.DoesNotExist:
+            return JsonResponse({'error': 'Course not found'}, status=404)
+        
+        try:
+            data = json.loads(request.body)
+        except:
+            return JsonResponse({'error': 'Invalid data'}, status=400)
+        
+        serializer = CourseSerializer(course, data=data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'message': 'Course updated successfully'}, status=200)
+        else:
+            return JsonResponse(serializer.errors, status=400)
+
+    def delete(self, request, course_id):
+        try:
+            course = course_details.objects.get(id=course_id)
+        except course_details.DoesNotExist:
+            return JsonResponse({'error': 'Course not found'}, status=404)
+        
+        
+        course.delete()
+        return JsonResponse({'message': 'Course deleted successfully'}, status=200)
+    
+class Student(View):
+    def get(self, request, student_roll):
+        student = student_details.objects.get(roll=student_roll)
+        serializer = StudentSerializer(student)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type="application/json")
+
+    def post(self, request):
+        data = request.body
+        stream = io.BytesIO(data)
+        pythonData = JSONParser().parse(stream)
+        serializer = StudentSerializer(data = pythonData)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Created'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        
+        json_data = JSONRenderer().render(serializer.error_messages)
+        return HttpResponse(json_data, content_type='application/json')
+
+    def put(self, request, student_roll):
+        try:
+            student=student_details.objects.get(roll=student_roll)
+        except student_details.DoesNotExist:
+            return JsonResponse({'error': 'Student not found'}, status =404)
+        
+        try:
+            data = json.loads(request.body)
+        except:
+            return JsonResponse({'error': 'Invalid data'}, status=400)
+        
+        serializer = StudentSerializer(student, data=data, partial = True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'message': 'Student updated successfully'}, status= 200)
+        else:
+            return JsonResponse(serializer.errors, status=400)
+    
+    def delete(self, request, student_roll):
+        try:
+            student = student_details.objects.get(roll=student_roll)
+        except student_details.DoesNotExist:
+            return JsonResponse({'error': 'Student not found'}, status=404)
+        
+        student.delete()
+        return JsonResponse({'message': 'Student deleted successfully'}, status=200)
 
 # def course_detail(request, pk):
 #     course = course_details.objects.get(id=pk)
--- Practice11_Django/serializer/serializer/urls.py
+++ Practice11_Django/serializer/serializer/urls.py
@@ -1,23 +1,25 @@
 from django.contrib import admin
 from django.urls import include, path
 from course import views
+from course.views import Course, Student
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # get and post
-    path('course-info/<int:pk>',views.course_detail),
-    path('course-info/',views.course_list),
-    path('course/<int:pk>/enrolled/',views.enrolled_students),
-    path('addStudent/',views.student_create),
-    path('addCourse/',views.course_create),
-    path('enrollStudent/',views.enroll_student), 
+    path('course-info/<int:pk>',Course.as_view()),
+    path('course-info/',Course.as_view()),
+    # path('course/<int:pk>/enrolled/',views.enrolled_students),
+    path('addStudent/',Student.as_view()),
+    path('addCourse/',Course.as_view()),
+    # path('enrollStudent/',views.enroll_student), 
 
     #update 
-    path('updateCourse/<str:course_id>/update',views.course_update),
-    path('updateStudent/<str:student_id>/update',views.student_update),
+    path('updateCourse/<str:course_id>/update',Course.as_view()),
+    path('updateStudent/<str:student_id>/update',Student.as_view),
 
     #delete
-    path('deleteCourse/<str:course_id>/delete',views.course_delete),
-    path('deleteStudent/<str:student_id>/delete',views.student_delete),
+    path('deleteCourse/<str:course_id>/delete',Course.as_view()),
+    path('deleteStudent/<str:student_id>/delete',Student.as_view),
 ] 
