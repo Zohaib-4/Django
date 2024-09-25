93a6d7e3c54a9e336ca5bd498ca71fa436122681
Zohaib <sameer.zohaib363@gmail.com>
2024-09-25 19:30:08 +0500

    practice 11

---
+++ Practice12_Django/pro/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Student
+from .serializer import student_serializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions 
+# Create your views here.
+
+class StudentModeViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = student_serializer
+    # authentication_classes = [BasicAuthentication]
+    authentication_classes = [SessionAuthentication]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [DjangoModelPermissions]
+    # permission_classes = [IsAdminUser]
