b0f86c16c327e73fec8b0d40ed6991c74205d754
Zohaib <sameer.zohaib363@gmail.com>
2024-10-01 21:10:06 +0500

    ../

--- Practice14_Django/practice/practice/settings.py
+++ Practice14_Django/practice/practice/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kccly@3w+n52x7cv^7%lcufti+--y&f8y_t1aoh_&m0&y%n!a3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://127.0.0.1:8000/'
+]
 
 
 # Application definition
